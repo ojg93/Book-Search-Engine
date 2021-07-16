4bfea1fafef64a6f167f694d33b6352b451375c5
Omari Grampus <ojgrampus@gmail.com>
2021-07-15 21:54:04 -0400

    edited login/signup forms

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/react-hooks';
-import { ADD_USER } from '../utils/mutations';
+
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -12,8 +11,6 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-  
-  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
