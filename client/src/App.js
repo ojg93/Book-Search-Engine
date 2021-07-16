3124ec7f64e04df71024838cda4d92545157c895
Omari Grampus <ojgrampus@gmail.com>
2021-07-15 20:28:48 -0400

    final push

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 
 const client = new ApolloClient({
+  cache:new InMemoryCache(),
   request: operation => {
     const token = localStorage.getItem('id_token');
 
