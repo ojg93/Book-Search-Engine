5410e14e14cd82a5389d5e32b75f711c7087fd81
Omari Grampus <ojgrampus@gmail.com>
2021-07-15 20:47:53 -0400

    edited login/signup form functions

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -10,7 +10,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [addUser, { error }] = useMutation(ADD_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
